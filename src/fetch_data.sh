52ec7025a6c9520e5ad9bef784eb9a30485b340d
luizamfsantos <luiza.marques_@hotmail.com>
2025-04-22 18:16:41 -0300

    script to fetch data

---
+++ src/fetch_data.sh
@@ -0,0 +1,3 @@
+curl -L -o data/raw/users_vs_bots.zip https://www.kaggle.com/api/v1/datasets/download/juice0lover/users-vs-bots-classification
+unzip -o data/raw/users_vs_bots.zip -d data/raw/
+rm data/raw/users_vs_bots.zip
