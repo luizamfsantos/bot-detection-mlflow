1a8d25a943e42dcf2e2f657ea2ff9c172e94c23b
luizamfsantos <luiza.marques_@hotmail.com>
2025-04-23 05:34:59 -0300

    update save and load data

--- src/data_processing.py
+++ src/data_processing.py
@@ -1,45 +1,23 @@
-import pickle
-from ucimlrepo import fetch_ucirepo
 import sys
 from pathlib import Path
+
 import pandas as pd
-sys.path.append('..')
 
-raw_data_path: str = 'data/raw/raw_data.pkl'
+sys.path.append("..")
 
-
-def save_dataset(
-    raw_data_path: str = raw_data_path,
-) -> None:
-    data = fetch_ucirepo(id=372)
-    with open(raw_data_path, 'wb') as f:
-        pickle.dump(data, f)
+raw_data_path: str = "data/raw/bots_vs_users.csv"
 
 
-def load_dataset(
-    raw_data_path: str = raw_data_path,
-) -> dict:
-    raw_data_path = Path(raw_data_path)
-    if not raw_data_path.exists():
-        save_dataset()
-    with open(raw_data_path, 'rb') as f:
-        data = pickle.load(f)
-    return data
+def save_dataset() -> None:
+    import subprocess
+
+    subprocess.run(["./src/fetch_data.sh"])
 
 
 def load_data(
     raw_data_path: str = raw_data_path,
-) -> tuple[pd.DataFrame, pd.DataFrame]:
-    data = load_dataset(raw_data_path)
-    X = data.data.features
-    y = data.data.targets
-    return X, y
-
-
-def load_metadata(
-    raw_data_path: str = raw_data_path,
-) -> dict:
-    data = load_dataset(raw_data_path)
-    metadata = data.metadata
-    metadata['variables'] = data.variables
-    return metadata
+) -> pd.DataFrame:
+    raw_data_path = Path(raw_data_path)
+    if not raw_data_path.exists():
+        save_dataset()
+    return pd.read_csv(raw_data_path)
