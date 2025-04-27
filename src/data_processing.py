c113f5aa37dfaefc557fa8b17799c37ff6582d6c
luizamfsantos <luiza.marques_@hotmail.com>
2025-04-27 18:18:05 -0300

    load data and test

--- src/data_processing.py
+++ src/data_processing.py
@@ -20,4 +20,10 @@ def load_data(
     raw_data_path = Path(raw_data_path)
     if not raw_data_path.exists():
         save_dataset()
-    return pd.read_csv(raw_data_path)
+    df_raw = pd.read_csv(raw_data_path, na_values=["Unknown"])
+    manually_delete_cols = ["has_domain", "has_short_name", "has_first_name"]
+    df_raw = df_raw.drop(columns=manually_delete_cols)
+    # update later
+    # for now, remove city
+    df_raw = df_raw.drop(columns=["city"])
+    return df_raw
